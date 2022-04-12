0797268f7d24bfd1351165f408f733602b2c86b9
Thomas Brettin <brettin@anl.gov>
2022-04-12 10:15:05 -0500

    container builder

---
+++ src/bootstrap.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+export IHOME=/lambda_stor/software/improve
+export IIL=${IHOME}/images
+export ICL=${IHOME}/containers
+DATE=$(date +%Y%m%d)
+NAME="${1:-default}" 
+
+mkdir -p $IHOME
+mkdir -p $IIL
+mkdir -p $ICL
+
+# singularity version 3.9.4
+echo "getting image"
+singularity build                \
+	$IIL/tensorflow-latest-gpu-${DATE}.sif         \
+	docker://tensorflow/tensorflow:latest-gpu
+
+echo "building sandbox"
+singularity build --sandbox      \
+        $ICL/${NAME}-tensorflow-latest-gpu-${DATE}  \
+        $IIL/tensorflow-latest-gpu-${DATE}.sif
+
+echo "logging into new container"
+singularity shell --nv --writable \
+	$ICL/${NAME}-tensorflow-latest-gpu-${DATE}
+
+## This would go in a new file for building the container
+# Python 3.8.10 
+# tf.__version__ 2.8.0
+
+# pip install pandas, matplotlib
+# mkdir -p /usr/local/improve/
+# cd /usr/local/improve
