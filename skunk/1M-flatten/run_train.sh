79de102a68ee40127709ae47b87fa1e3131df3bc
Thomas Brettin <brettin@anl.gov>
2022-05-18 16:31:30 -0500

    cleaned up args to EXE

--- skunk/1M-flatten/run_train.sh
+++ skunk/1M-flatten/run_train.sh
@@ -4,7 +4,7 @@
 # arg2 = --in_vali
 # arg3 = GPU Device
 
-EXE="singularity exec --nv /lambda_stor/software/improve/containers/regress_transformer-tensorflow-latest-gpu-20220412 python smiles_regress_transformer.py --in_train $1 --in_vali $2"
+EXE="singularity exec --nv /lambda_stor/software/improve/containers/regress_transformer-tensorflow-latest-gpu-20220412 python smiles_regress_transformer.py "
 
 echo $EXE
 echo ""
@@ -16,11 +16,11 @@ echo $base
 echo ""
 
 mkdir -p DIR.$base
-/bin/cp -n $1 DIR.$base
-/bin/cp -n $2 DIR.$base
-/bin/cp smiles_regress_transformer.py  DIR.$base
+/bin/cp -n $1 DIR.$base/$1
+/bin/cp -n $2 DIR.$base/$2
+/bin/cp smiles_regress_transformer.py  DIR.$base/
 
-cd DIR.$base
-CUDA_VISIBLE_DEVICES=$3 $EXE --ep 200 --in_train $1 --in_vali $2 > output.log.$$ 2>&1
+cd DIR.$base/
+CUDA_VISIBLE_DEVICES=$3 $EXE --ep 400 --in_train $1 --in_vali $2 > output.log.$$ 2>&1
 cd ..
 #rm -r DIR.$1
