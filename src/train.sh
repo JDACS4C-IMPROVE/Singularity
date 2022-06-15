b5b457bd50af07a4175bb4cc276274ee8940a37f
brettin <brettin@.anl.gov>
2022-06-15 15:14:08 -0500

    updated train.sh

--- src/train.sh
+++ src/train.sh
@@ -3,4 +3,13 @@
 # Launches model training from within the container
 # TODO: define what args can be passed
 
-drp_model.py --args $ARGS
+# Anything that needs to be done within the container before
+# the model can be trained
+
+
+
+PARAMS_FILE="model_params.txt"
+ARGS="--config_file ${PARAMS_FILE}"
+drp_model.py --args $ARG
+
+
