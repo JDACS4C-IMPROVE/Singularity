8b81067e7239e86b7b54bfe9f9d0e658ae71e613
brettin <brettin@.anl.gov>
2022-06-15 14:38:32 -0500

    singularity model interface commands

---
+++ src/infer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Launches model for inferencing from within the container
+# TODO: define what args can be passed
+
+infer.py --args $ARGS
---
+++ src/train.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Launches model training from within the container
+# TODO: define what args can be passed
+
+drp_model.py --args $ARGS
