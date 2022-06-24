888cc337e2672fa22ca46ff3bf10ccf543b1eafb
brettin <brettin@.anl.gov>
2022-06-24 16:46:28 -0500

    standardizing

--- src/bootstrap.sh
+++ src/bootstrap.sh
@@ -8,16 +8,16 @@ DATE=$(date +%Y%m%d)
 NAME="${1:-default}"
 
 
-IHOME=${IHOME:-/home/brettin/software/improve}
-export ${IHOME}
+IHOME="${IHOME:-/homes/brettin/software/improve}"
+export IHOME=${IHOME}
 export IIL=${IHOME}/images
-export ICL=${IHOME}/containers
+export ISL=${IHOME}/sandboxes
 export IDL=${IHOME}/definitions
 
 
 mkdir -p $IHOME
 mkdir -p $IIL
-mkdir -p $ICL
+mkdir -p $ISL
 mkdir -p $IDL
 
 
@@ -26,6 +26,7 @@ mkdir -p $IDL
 
 # For tensorflow
 TAG=":2.4.3-gpu"
+TAG=":2.8.2-gpu"
 IMAGE="tensorflow"${TAG}
 
 echo "getting image: $IMAGE"
@@ -33,19 +34,21 @@ singularity build                \
 	$IIL/$IMAGE-${DATE}.sif         \
 	docker://tensorflow/$IMAGE
 
-echo "building sandbox frmo image $IIL/${IMAGE}-${DATE}.sif"
+echo "building sandbox from image $IIL/${IMAGE}-${DATE}.sif"
+echo "building sandbox at ${ISL}"
+
 singularity build --fakeroot --sandbox      \
-        $ICL/${NAME}-$IMAGE-${DATE}  \
+        $ISL/${NAME}-$IMAGE-${DATE}  \
         $IIL/${IMAGE}-${DATE}.sif
 
 #echo "building sandbox from definition file ${IDL}/"
 #singularity build --fakeroot --sandbox      \
-#	$ICL/${NAME}-$IMAGE-${DATE}         \
+#	$ISL/${NAME}-$IMAGE-${DATE}         \
 #	$IDL/${IMAGE}-${DATE}.def
 
 echo "logging into new container"
 singularity shell --nv --fakeroot --writable \
-	$ICL/${NAME}-${IMAGE}-${DATE}
+	$ISL/${NAME}-${IMAGE}-${DATE}
 
 ## This would go in a new file for building the container
 # Python 3.8.10 
--- src/train.sh
+++ src/train.sh
@@ -4,7 +4,7 @@
 # arg 2 CANDLE_DATA_DIR
 # arg 3 CANDLE_CONFIG
 
-CANDLE_MODEL=/usr/local/Benchmarks/Pilot1/Attn/attn_baseline_keras2.py
+CANDLE_MODEL=/usr/local/Benchmarks/Pilot1/Uno/uno_baseline_keras2.py
 
 if [[ "$#" -ne 3 ]] ; then
     echo "Illegal number of parameters"
@@ -16,13 +16,14 @@ CUDA_VISIBLE_DEVICES=$1
 CANDLE_DATA_DIR=$2
 CANDLE_CONFIG=$3
 
-CMD="python $CANDLE_MODEL --config_file $CANDLE_CONFIG"
+CMD="python ${CANDLE_MODEL} --config_file ${CANDLE_CONFIG}"
 
-echo "using CUDA_VISIBLE_DEVICES $CUDA_VISIBLE_DEVICES"
-echo "using containers at $ICL"
 echo "using container pilot1.0_5_1-tensorflow:2.8.2-gpu-20220617"
+echo "using CUDA_VISIBLE_DEVICES ${CUDA_VISIBLE_DEVICES}"
+echo "using CANDLE_DATA_DIR ${CANDLE_DATA_DIR}"
+echo "using CANDLE_CONFIG ${CANDLE_CONFIG}"
 echo "running command ${CMD}"
 
-CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES CANDLE_DATA_DIR=$CANDLE_DATA_DIR $CMD
+CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES} CANDLE_DATA_DIR=${CANDLE_DATA_DIR} $CMD
 
 
