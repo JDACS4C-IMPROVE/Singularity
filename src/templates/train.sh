90379cab57a983ba893f9f7551258e49e11bee57
brettin <brettin@.anl.gov>
2022-11-08 13:22:39 -0600

    fixed bash syntax in if elif else

--- src/templates/train.sh
+++ src/templates/train.sh
@@ -1,35 +1,39 @@
-#############################################################################################################
-### THIS IS A TEMPLATE FILE. SUBSTITUTE '???' ENTRIES WITH THE APPROPRiATE INFORMATION FOR YOUR CONTAINER ###
-#############################################################################################################
 
 #!/bin/bash
+
+#########################################################################
+### THIS IS A TEMPLATE FILE. SUBSTITUTE #PATH# WITH THE MODEL EXECUTABLE.
+#########################################################################
+
 
 # arg 1 CUDA_VISIBLE_DEVICES
 # arg 2 CANDLE_DATA_DIR
 # arg 3 CANDLE_CONFIG
 
 ### Path to your CANDLEized model's main Python script###
-CANDLE_MODEL=#PATH#
+CANDLE_MODEL=#/usr/local/GraphDRP/graphdrp_baseline_pytorch.py#
 
-# Reading command line arguments
-CUDA_VISIBLE_DEVICES=$1
-CANDLE_DATA_DIR=$2
-CANDLE_CONFIG=$3
+if [ $# -lt 2 ] ; then
+	echo "Illegalnumber of paramaters"
+	exit -1
 
-# Check if CANDLE_CONFIG exists if user passed this parameter and prepare 
-if [ ! -z "${CANDLE_CONFIG}" ]; then
-        if [ ! -f "$CANDLE_CONFIG" ]; then
-            echo "Cannot read configuration file! If you want to run model with default parameters leave third option empty."
-			exit -1
-		else
-			CMD="python3 ${CANDLE_MODEL} --config_file ${CANDLE_CONFIG}"
-        fi
+elif [ $# -eq 3 ] ; then
+  CUDA_VISIBLE_DEVICES=$1
+  CANDLE_DATA_DIR=/candle_data_dir/$2
+  CANDLE_CONFIG=$3
+  CMD="python ${CANDLE_MODEL} --config_file $CANDLE_CONFIG"
+  echo "CMD = $CMD"
+
 else
-	CMD="python3 ${CANDLE_MODEL}"
+  echo "num args is greather than 3 $#"
+  # the candle config is converted to command line parameters
+  CUDA_VISIBLE_DEVICES=$1 ; shift
+  CANDLE_DATA_DIR=/candle_data_dir/$1 ; shift
+  CMD="python ${CANDLE_MODEL} $@"
+  echo "CMD = $CMD"
 fi
 
 # Display runtime arguments
-echo "using container "
 echo "using CUDA_VISIBLE_DEVICES ${CUDA_VISIBLE_DEVICES}"
 echo "using CANDLE_DATA_DIR ${CANDLE_DATA_DIR}"
 echo "using CANDLE_CONFIG ${CANDLE_CONFIG}"
@@ -37,5 +41,3 @@ echo "running command ${CMD}"
 
 # Set up environmental variables and execute model
 CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES} CANDLE_DATA_DIR=${CANDLE_DATA_DIR} $CMD
-
-
