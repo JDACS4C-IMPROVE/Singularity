ffec86be19a157dcb0088c4cf2b07b33e6216bd2
brettin <brettin@.anl.gov>
2022-12-10 10:18:09 -0600

    add bind to singularity shell

--- src/login.sh
+++ src/login.sh
@@ -1,3 +1,3 @@
 echo "logging into new container"
 source "../config/run.config"
-singularity shell --nv --fakeroot --writable $1
+singularity shell --nv --fakeroot --writable --bind $IDD:/candle_data_dir $1
