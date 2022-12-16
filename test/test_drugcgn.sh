06cf1dae63fe940612ff6bbd9ac6d0fb6b969a08
Thomas Brettin <brettin@lambda7.cels.anl.gov>
2022-12-16 13:42:01 -0600

    new file

---
+++ test/test_drugcgn.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -x
+
+GPUID="${1:-"2"}"
+BUILD_DATE=${DATE:-"20221210"}
+
+IHOME=/home/brettin/Singularity
+IMPROVE_DATA_DIR=/home/brettin/improve_data_dir
+
+CONTAINER="DrugGCN-DrugGCN:0.0.1-$BUILD_DATE"
+MODEL_FILE="druggcn_default_model.txt"
+
+# FRAGILE
+# singularity exec --nv --bind $IMPROVE_DATA_DIR:/candle_data_dir $IHOME/sandboxes/${CONTAINER}/ cp /usr/local/DrugGCN/$MODEL_FILE /candle_data_dir
+
+singularity exec --nv --bind $IMPROVE_DATA_DIR:/candle_data_dir $IHOME/sandboxes/${CONTAINER}/ train.sh 3 /candle_data_dir
+singularity exec --nv --bind $IMPROVE_DATA_DIR:/candle_data_dir $IHOME/sandboxes/${CONTAINER}/ train.sh 3 /candle_data_dir ${MODEL_FILE}
+singularity exec --nv --bind $IMPROVE_DATA_DIR:/candle_data_dir $IHOME/sandboxes/${CONTAINER}/ train.sh 3 /candle_data_dir --config_file ${MODEL_FILE}
+singularity exec --nv --bind $IMPROVE_DATA_DIR:/candle_data_dir $IHOME/sandboxes/${CONTAINER}/ train.sh 3 /candle_data_dir ${MODEL_FILE} --experiment_id EXP001 --run_id RUN001
+
