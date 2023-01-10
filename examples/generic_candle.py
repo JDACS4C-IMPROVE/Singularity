1c5785cf399b38280e8797aa16a2ba5c10863917
Andreas Wilke <wilke@anl.gov>
2023-01-10 17:42:48 -0600

    Update generic_candle.py

--- examples/generic_candle.py
+++ examples/generic_candle.py
@@ -55,6 +55,18 @@ def run(params):
     # infer using model
     # etc
     print("running third party code")
+    
+    # Dumping results into file, workflow requirement
+    val_scores = { 'key' : 'val_loss' ,
+                  'value' : metrics['val_loss'],
+                  'val_loss':metrics['val_loss'], 
+                  'pcc':metrics['pcc'], 
+                  'scc':metrics['scc'], 
+                  'rmse':metrics['rmse']}
+
+    with open(params['output_dir'] + "/scores.json", "w", encoding="utf-8") as f:
+        json.dump(val_scores, f, ensure_ascii=False, indent=4)
+    
     return metrics                                              # metrics is used by the supervisor when running
                                                                 # HPO workflows (and possible future non HPO workflows)
 
