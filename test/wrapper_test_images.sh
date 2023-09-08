4179d9040ea6b8a7f1510527a81a06847c253866
Thomas Brettin <brettin@anl.gov>
2023-09-08 06:14:46 -0500

    added redirect to log file

--- test/wrapper_test_images.sh
+++ test/wrapper_test_images.sh
@@ -2,12 +2,14 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 GPUID="${1:-0}"
+BUILD_DATE="${BUILD_DATE:-"20230000"}"
 
 for n in $(ls $SCRIPT_DIR/../build/) ; do
 	echo "$n"
 	
 	CMD="$SCRIPT_DIR/test_image.sh $GPUID $n"
-	echo "$CMD"
+	echo "$CMD > $BUILD_DATE.$n.test.log 2>&1"
+	# CMD &
 	pids[${i}]=$!
 
         GPUID=$(( GPUID+1 ))
