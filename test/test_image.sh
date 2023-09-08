d841f1afe91ddec09b7c8714edb1909d9bfdeca7
Thomas Brettin <brettin@anl.gov>
2023-09-08 05:57:18 -0500

    use build dir to find images

--- test/test_image.sh
+++ test/test_image.sh
@@ -6,7 +6,7 @@ GPUID=$1
 CONTAINER=$2
 IMPROVE_DATA_DIR="${3:-"/tmp"}"
 
-CMD="singularity exec --nv --bind $IMPROVE_DATA_DIR:/candle_data_dir $$SCRIPT_DIR/../images/${CONTAINER}/ train.sh $GPUID /candle_data_dir"
+CMD="singularity exec --nv --bind $IMPROVE_DATA_DIR:/candle_data_dir $$SCRIPT_DIR/../build/${CONTAINER}/ train.sh $GPUID /candle_data_dir"
 
 echo "$(date +%Y-%m-%d" "%H:%M:%S) START" 
 
