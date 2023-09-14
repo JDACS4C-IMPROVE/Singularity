5d1e77575b38dd5209c0232c170ea3d2ba52d6b1
Rajeev Jain <rajeeja@@gmail.com>
2023-09-14 16:16:13 -0700

    o update to python build container cmd wrapper

--- test/build_container.py
+++ test/build_container.py
@@ -1,67 +1,80 @@
 import os
+import subprocess
+import sys
+import argparse
+import os
+import subprocess
 
-def build_singularity_container(model_name, singularity_dir):
+def build_singularity_container(model_name, definitions_file, deployment_dir_file, options):
     """
-    Build singularity container from model_name and singularity_dir
-    Assumes definition file is in singularity_dir/definitions
+    Build singularity container deployment_dir_file from model_name and definitions_file
     """
+
+    cmd = "singularity build " + options + deployment_dir_file + " " + definitions_file
+    print("Running command:", cmd)
     
-    print("Building singularity container")
-    image_path = singularity_dir + "/images/" + model_name + ".sif"
-    print("image_path:", image_path)    
+    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+
+    print("Standard Output:")
+    print(stdout.decode("utf-8"))
+
+    print("\nStandard Error:")
+    print(stderr.decode("utf-8"))
     
-    if os.path.isfile(image_path):
-        print("Singularity container already exists")
-        return
+    return deployment_dir_file
+
+
+
+if __name__ == "__main__":
+    
+    
+    parser = argparse.ArgumentParser(description="Build Singularity container. Launch from Singularity/test as Eg. 1: python build_container.py --model_name DeepTTC  .OR. Eg.2: python build_container.py --model_name GraphDRP --definitions_file /PathTo/GraphDRP.def --deployment_dir_file /PathTo/GraphDRP.sif")
+    parser.add_argument("--model_name", help="Name of the model", default="GraphDRP")
+    parser.add_argument("--singularity_dir", help="Path to the Singularity directory", default="../")
+    parser.add_argument("--definitions_file", help="Path to the Singularity definition file", default="")
+    parser.add_argument("--deployment_dir_file", help="Path to the deployment directory including .sif filename", default="")
+    parser.add_argument("--options", help="Additional options for Singularity build", default="--fakeroot --disable-cache ")
+
+    args = parser.parse_args()
+    
+    model_name = args.model_name
+    deployment_dir_file = args.deployment_dir_file
+    definitions_file = args.definitions_file
+    singularity_dir = args.singularity_dir
+    
+    if deployment_dir_file == "":
+        deployment_dir_file = singularity_dir + "/images/" + model_name + ".sif"
+        if os.path.isfile(deployment_dir_file):
+            print("Singularity container already exists: ", deployment_dir_file)
+            sys.exit(1)
+        else:
+            print("Singularity container does not exist", deployment_dir_file)
     else:
-        print("Singularity container does not exist")
-        image_path = build(image_path, singularity_dir)
+        print("Trying to create singularity container:", deployment_dir_file)
         
-    return image_path
+    if definitions_file == "":
+        definitions_file = singularity_dir + "/definitions/" + model_name + ".def"
+        if os.path.isfile(definitions_file):
+            print("Definition file exists:", definitions_file)
+        else:
+            print("Definition file does not exist:", definitions_file)
+            sys.exit(1)
+    else:
+        if os.path.isfile(definitions_file):
+            print("Definition file exists:", definitions_file)
+        else:
+            print("Definition file does not exist:", definitions_file)
+            sys.exit(1)
+    
+    args.deployment_dir_file = deployment_dir_file
+    args.definitions_file = definitions_file
+    
+    print(args)
 
-
+    # call build_singularity_container
+    image_path = build_singularity_container(args.model_name, args.definitions_file, args.deployment_dir_file, args.options)
     
-def build(image_path, singularity_dir):
-    """build singularity container"""
+    print("Build completed, created, IMAGE:", image_path)
     
-    os.chdir(singularity_dir)
-    # launch the command to build the singularity container
-    print("Building singularity container:-", image_path)
     
-    options = "--fake-root --disable-cache "
-    
-    cmd = "singularity build " + options + image_path + " " + singularity_dir + "/definitions/" + model_name + ".def"
-    
-    return image_path
-
-    # # Check if singularity is installed
-    # if not check_singularity_installed():
-    #     print("Singularity is not installed")
-    #     return
-    
-    # # Check if singularity_dir exists
-    # if not check_file_exists(singularity_dir):
-    #     print("Singularity directory does not exist")
-    #     return
-    
-    # # Check if model_name is valid
-    # if not model_name:
-    #     print("Model name is not valid")
-    #     return
-    
-    # # Check if model_name is in singularity_dir
-    # if not check_file_exists(os.path.join(singularity_dir, model_name)):
-    #     print("Model name is not in singularity directory")
-    #     return
-    
-    # # Check if model_name.sif already exists
-    # if check_file_exists(os.path.join(singularity_dir, model_name + ".sif")):
-    #     print("Model name already exists")
-    #     return
-    
-    # # Build singularity container
-    # command = ["singularity", "build", os.path.join(singularity_dir, "/images/" model_name + ".img"), ""]
-    # print(" ".join(command))
-    # send_terminal_output_to_file(command, os.path.join(singularity_dir, "build_singularity_container.txt"))
-    # print("Done building singularity container")
-    # return
